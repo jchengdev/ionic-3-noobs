7893c31cc424c556281fe4161f9c86e354e7d78c
jchengdev <30513739+jchengdev@users.noreply.github.com>
2018-06-12 22:43:50 -0300

    images added

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,12 +14,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  public nomeUsuario: string = 'nome_user';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  public somaDoisNumeros(num1: number, num2: number): void {
+    alert('minha função funciona: '+(num1+num2));
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedPage');
+    // this.somaDoisNumeros(1,2);
   }
 
 }
